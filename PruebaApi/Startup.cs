177f802ddc8f44549af87093f79702f01e8038d4
JoseMedina <JoseMedina@DESKTOP-J6GQEMM>
2023-05-10 17:25:33 +0200

    proyecto terminado

--- PruebaApi/Startup.cs
+++ PruebaApi/Startup.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNet.OData.Builder;
-using Microsoft.AspNet.OData.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,14 +6,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OData.Edm;
-using ODataPrueba.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ODataPrueba
+namespace PruebaApi
 {
     public class Startup
     {
@@ -24,30 +20,12 @@ namespace ODataPrueba
             Configuration = configuration;
         }
 
-        //CREAMOS ESTE METODO PARA ENVIAR LAS CLASES QUE TENEMOS
-        public static IEdmModel GetEdmModel()
-        {
-            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
-            builder.EntitySet<Libros>("Libros");
-            builder.EntitySet<Autores>("Autores");
-            return builder.GetEdmModel();
-        }
-
         public IConfiguration Configuration { get; }
 
-        //AÑADIMOS LOS SERVICE QUE NOS HACE FALTA
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddOData();
-            services.AddMvc(option=>option.EnableEndpointRouting=false);
-
-            services.AddDbContext<BibliotecaODataContext>();
-            
-
-
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,13 +35,6 @@ namespace ODataPrueba
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseMvc(p =>
-            {
-                p.Select().Expand().Filter().OrderBy().MaxTop(100).Count();
-                p.MapODataServiceRoute("odata", "odata", GetEdmModel());
-            }
-            );
 
             app.UseHttpsRedirection();
 
